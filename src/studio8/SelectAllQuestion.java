c623379301907e0f252a06d10d82aa9cede0fbb3
jackh <jackhhollings@gmail.com>
2023-11-29 15:26:55 -0600

    studio 8  final

--- src/studio8/SelectAllQuestion.java
+++ src/studio8/SelectAllQuestion.java
@@ -1,20 +1,29 @@
 package studio8;
 
 public class SelectAllQuestion extends MultipleChoiceQuestion {
+	
 
-	public SelectAllQuestion(String prompt, String answer, String[] choices) {
-		//Hint: 1 point per choice
-		//FIXME
+	public SelectAllQuestion(String prompt, String answer, int totPoints, String[] choices) {
+		super(prompt,answer,answer.length() * 3,choices);
 	}
 	
 	public int checkAnswer(String givenAnswer) {
-		//FIXME Should return partial credit (if earned)!
-		return 0;
+		int score = 0;
+		for(int i = 0; i < givenAnswer.length(); i ++) {
+			if(this.getAnswer().contains((Character.toString(givenAnswer.charAt(i))))) {
+				score +=3; //three points for contain correct answer
+			}
+			else {
+				score -=1;
+			}
+		}
+		if(score < 0) score = 0; //set negative score to 0
+		return score;
 	}
 	
 	public static void main(String[] args) {
 		String[] choices = {"instance variables", "git", "methods", "eclipse"};
-		Question selectAll = new SelectAllQuestion("Select all of the following that can be found within a class:", "13", choices);
+		Question selectAll = new SelectAllQuestion("Select all of the following that can be found within a class:", "13", 0,choices);
 		selectAll.displayPrompt();
 		System.out.println(selectAll.checkAnswer("hi")); //no credit
 		System.out.println(selectAll.checkAnswer("13")); //full credit
